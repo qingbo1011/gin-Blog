1fb8c22eab87d26d8a970b68c3a5b5f4f9209c7c
qingbo1011 <61835535+qingbo1011@users.noreply.github.com>
2022-07-08 16:02:17 +0800

    api v1

--- route/route.go
+++ route/route.go
@@ -1,9 +1,21 @@
 package route
 
-import "github.com/gin-gonic/gin"
+import (
+	v1 "gin-Blog/api/v1"
+
+	"github.com/gin-gonic/gin"
+)
 
 func NewRoute() *gin.Engine {
 	r := gin.Default() //gin.Default()和gin.New()的区别：gin.Default()默认使用logger和recovery中间件
+
+	apiV1 := r.Group("/api/v1")
+	{
+		apiV1.GET("/tags", v1.GetTags)
+		apiV1.POST("/tags", v1.AddTag)
+		apiV1.PUT("/tags/:id", v1.EditTag) // gin绑定URI
+		apiV1.GET("/tags/:id", v1.DeleteTag)
+	}
 
 	return r
 }
